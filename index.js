342d6c528ae5d4f9366c4ef0af44e35ef45c8fee
Tanmaya Behera <tanmaya.behera@abbott.com>
2021-07-29 16:17:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+require('dotenv').config();
+const http = require("http");
+const fs = require("fs");
+var requests = require("requests");
+
+
+const homeFile = fs.readFileSync("homePage.html", "utf-8");
+
+
+const replaceVal = (tempVal, orgVal) => {
+  let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+  temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+  temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+  temperature = temperature.replace("{%location%}", orgVal.name);
+  temperature = temperature.replace("{%country%}", orgVal.sys.country);
+  temperature = temperature.replace("{%tempstatus%}", orgVal.weather[0].main);
+
+  return temperature;
+};
+
+const server = http.createServer((req, res) => {
+  if (req.url == "/") {
+    requests(
+      `http://api.openweathermap.org/data/2.5/weather?q=Bhubaneswar&units=metric&appid=252e3cc1d8be532195c4a82b607c2f4d`
+    )
+      .on("data", (chunk) => {
+        const objdata = JSON.parse(chunk);
+        const arrData = [objdata];
+        const realTimeData = arrData
+          .map((val) => replaceVal(homeFile, val))
+          .join("");
+        res.write(realTimeData);
+      })
+      .on("end", (err) => {
+        if (err) return console.log("connection closed due to errors", err);
+        res.end();
+      });
+  } else {
+    res.end("File not found");
+  }
+});
+
+server.listen(8000);
